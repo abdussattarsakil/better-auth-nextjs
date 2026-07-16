da7cd2060b52f851dfa4ac402cb32bdce3a87425
abdussattarsakil <abdussattarsakil208@gmail.com>
2026-07-16 14:30:51 +0600

    tuki taki ui

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,15 @@
+import { Button } from "@heroui/react";
+import Link from "next/link";
+import {EyesLookLeft} from '@gravity-ui/icons';
 
 export default function Home() {
   return (
-   <div>
-
+   <div className="text-center">
+    <h1 className="text-2xl text-amber-50 font-extrabold">Learn BetterAuth MongoDb Adapter</h1>
+    <h2 className="font-mono text-3xl text-amber-100">Learn:Authentication</h2>
+    <Link href={"/auth/signup"}>
+    <Button variant="primary">Sign Up <EyesLookLeft></EyesLookLeft></Button>
+    </Link>
    </div>
   );
 }
